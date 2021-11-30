6ecb8d05a0edb3d1bd96d25ba0286d8faa7ecf69
raven <92213034+mindlesssapien@users.noreply.github.com>
2021-11-30 11:39:21 +0530

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -1,10 +1,10 @@
 def binary():
     print("Keep it logically awesome.")
-
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
-
-  #print(quotes)
+    
+    #f = open("quotes.txt")
+    #quotes = f.readlines()
+    #f.close()
+    
+    #print(quotes)
     #if__name__== "__main__":
 binary()
